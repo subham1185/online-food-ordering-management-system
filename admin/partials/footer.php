b74fe23d58d3184973e70eee5f1cff7a3418e3c7
subham pathania <63285681+subham1185@users.noreply.github.com>
2022-05-24 15:27:38 +0530

    Update footer.php

--- admin/partials/footer.php
+++ admin/partials/footer.php
@@ -1,7 +1,7 @@
 <!--footer section start -->
 <div class="footer">
             <div class="wrapper">
-               <p class="text-center">All Rights Reserved,---, Developed by <a href="#"> Subham Pathania </a> </p>
+               <p class="text-center">All Rights Reserved,---, Developed by <a href="#"> Subham Pathania, Harshdeep Singh, Simrandeep Singh, Dilpreet Singh </a> </p>
             </div>
         </div>
         <!--footer section end -->
